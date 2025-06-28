3c887c0fb8d2d2b889f19ee88549fb37509566aa
Larry Saytee <saytee.software@web-developer.one>
2025-06-28 21:53:40 +0000

    add a border to "Verified Members" box, "Secure Accounts" box, "24/7 Sup

--- src/components/home/security-privacy.tsx
+++ src/components/home/security-privacy.tsx
@@ -34,7 +34,7 @@ export function SecurityPrivacy() {
             return (
               <div
                 key={index}
-                className="flex flex-col items-center p-6"
+                className="flex flex-col items-center rounded-lg border p-6"
               >
                 <div className="mb-4 flex h-16 w-16 items-center justify-center rounded-full bg-primary/10 text-primary">
                   <Icon className="h-8 w-8" />

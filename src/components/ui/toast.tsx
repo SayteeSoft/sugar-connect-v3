6ffc89b0e961b86bd2722789c3b885aee32a57c2
Larry Saytee <saytee.software@web-developer.one>
2025-06-29 00:54:25 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -16,7 +17,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed top-20 right-0 z-[100] flex max-h-screen w-full flex-col p-4 md:max-w-[420px]",
+      "fixed top-20 right-0 z-[100] flex max-h-screen w-full flex-col p-4 max-w-[420px]",
       className
     )}
     {...props}

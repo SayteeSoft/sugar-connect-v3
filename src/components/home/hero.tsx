3e08ee298450ef4a0027ba90b407f7f191732793
Larry Saytee <saytee.software@web-developer.one>
2025-06-30 01:29:31 +0000

    make "SD Connect is Sugar Daddy Connect" larger font size just a little

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -24,7 +24,7 @@ export function Hero() {
         <h1 className="font-headline text-7xl font-bold tracking-tight md:text-9xl">
           SD Connect
         </h1>
-        <p className="text-xl text-gray-300 md:text-2xl">SD Connect is Sugar Daddy Connect</p>
+        <p className="text-2xl text-gray-300 md:text-3xl">SD Connect is Sugar Daddy Connect</p>
         <p className="max-w-2xl text-lg text-gray-200 md:text-xl">
           An exclusive platform for ambitious and attractive individuals
         </p>

c02fb21477497cecd6ae470d51c151d3d7f51066
Larry Saytee <saytee.software@web-developer.one>
2025-06-30 12:44:48 +0000

    change "SD Connect is Sugar Daddy Connect" to "SD Connect is 'Sugar Dadd

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -25,7 +25,7 @@ export function Hero() {
         <h1 className="font-headline text-7xl font-bold tracking-tight md:text-9xl">
           SD Connect
         </h1>
-        <p className="text-3xl text-gray-300 md:text-4xl">SD Connect is Sugar Daddy Connect</p>
+        <p className="text-3xl text-gray-300 md:text-4xl">SD Connect is 'Sugar Daddy' Connect</p>
         <p className="max-w-2xl text-lg text-gray-200 md:text-xl">
           An exclusive platform for ambitious and attractive individuals
         </p>
